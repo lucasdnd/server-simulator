3f66214edc49130ab8ed0158e60c2aac4728ced0
Lucas <lucasdnd@gmail.com>
2015-09-25 17:43:48 -0300

    Added the project

---
+++ html/src/com/lucasdnd/serversimulator/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.lucasdnd.serversimulator.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.lucasdnd.serversimulator.ServerSimulator;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new ServerSimulator();
+        }
+}
