4fc3becbe58c49f964d81aa1931fc7115e7fceed
Lucas <lucasdnd@gmail.com>
2015-09-25 18:41:26 -0300

    Added desktop project

---
+++ desktop/src/com/lucasdnd/serversimulator/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.lucasdnd.serversimulator.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.lucasdnd.serversimulator.ServerSimulator;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 960;
+		config.height = 640;
+		new LwjglApplication(new ServerSimulator(), config);
+	}
+}
